f1f3bdd04070ab7837c643cc2ae41e554cbbe3b1
olucheye <seyefree2rhyme@yahoo.com>
2020-04-14 17:39:18 +0100

    html & css structure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 setTimeout(bitcoinPrices, 1000);
 
 //bitcoinPrices();
@@ -8,32 +9,41 @@ async function bitcoinPrices(){
         let res = await fetch(url);
         const data = await res.json(); // passing out response in Json
 
-        let updateTime = data.time.updated; 
+        let updateTime = data.time.updated; // TIme at which the data was retrieved
 
-        for(let coin in data.bpi){
+        // Also add, disclaimer at the bottom of the page.
+
+        // Style the Div's to make more space & stylish.
+
+        //dynamic array that creates DOM content
+        for(let currency in data.bpi){
             // Loop through each coin & pass out its properties.
-            let props = data.bpi[coin];
+            let props = data.bpi[currency];
 
+            //create DOM elements
             let div = document.createElement('div');
             let h2 = document.createElement('h2');
             let p = document.createElement('p');
             
             //append elements to each other
             document.querySelector(".row").appendChild(div);
-            div.classList.add('col-lg-4');
+            div.classList.add('col-12');
             div.classList.add('coinBlock');
-            div.textContent = coin;
-
-            h2.textContent = props.description;
-            div.appendChild(h2);
+            div.textContent = currency;
             
+            //updated time
+            document.querySelector('#time').textContent = updateTime;
+
+            div.appendChild(h2);
+            //h2.textContent = props.description;
 
             h2.appendChild(p);
             p.textContent = props.code + ' ' + props.rate;
         };
     } catch (error) {
-        console.log(error);
+        docuement.querySelector('div').innerHtml = "<strong>An error occured, Please try again later!<</strong>"
         return false;
     }
 };
  
+
